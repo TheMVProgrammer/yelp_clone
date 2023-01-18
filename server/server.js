9b7038c2d3269c9c833f9e856bc003011d915e33
TheMVProgrammer <franklin0415@hotmail.com>
2023-01-18 15:16:12 -0400

    Finishing up routes

--- server/server.js
+++ server/server.js
@@ -1,14 +1,68 @@
 require('dotenv').config();
 
 const express = require("express");
-
+const morgan = require("morgan");
 const app = express();
 
-app.get("/getRestaurants", (req, res) => {
-    console.log("Get all restaurants");
+app.use(express.json());
+
+//Get all restaurants
+app.get("/api/v1/restaurants", (req, res) => {
+    console.log("Route handler ran");
+    res.status(200).json({
+        status: "success",
+        data: {
+            restaurant: ["Mcdonalds", "Wendys"],
+        },        
+    });
+});
+
+
+//Get a restaurant
+
+app.get("/api/v1/restaurants/:id", (req, res) => {
+    res.status(200).json({
+        status: "success",
+        data: {
+            restaurant: "McDonalds"
+        }
+    });
+});
+
+//Create a restaurant
+
+app.post("/api/v1/restaurants", (req, res) => {
+    res.status(201).json({
+        status: "success",
+        data: {
+            restaurant: "McDonalds"
+        }
+    });
+});
+
+//Update restaurants
+
+app.put("/api/v1/restaurants/:id", (req, res) => {
+    console.log(req.params.id);
+    console.log(req.body);
+    res.status(200).json({
+        status: "success",
+        data: {
+            restaurant: "McDonalds"
+        }
+    });
+});
+
+//Delete restaurants
+
+app.delete("/api/v1/restaurants/:id", (req, res) => {
+    res.status(204).json({
+        status: "success",
+    });
 });
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
     console.log(`Server is up and listening on port ${port}`);
 });
+
