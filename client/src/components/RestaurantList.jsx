1e472009f6b9d21c5d5f0fc71bb295e23bede0dd
TheMVProgrammer <franklin0415@hotmail.com>
2023-01-22 16:11:40 -0400

    Deleting restaurant

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -19,42 +19,53 @@ const RestaurantList = (props) => {
         
     }, []);
 
-  return (
-    <div className="list-group">
-        <table className="table table-striped table-dark">
-            <thead>
-                <tr>
-                    <th scope="col">Restaurant</th>
-                    <th scope="col">Location</th>
-                    <th scope="col">Price Range</th>
-                    <th scope="col">Ratings</th>
-                    <th scope="col">Edit</th>
-                    <th scope="col">Delete</th>
-                </tr>
-            </thead>
-            <tbody>
-                {restaurants && restaurants.map(restaurant => (
-                    <tr key={restaurant.restaurant_id}>
-                        <td>{restaurant.name}</td>
-                        <td>{restaurant.location}</td>
-                        <td>{"$".repeat(restaurant.price_range)}</td>
-                        <td>Rating</td>
-                        <td>
-                        <button className="btn btn-warning">
-                            Update
-                        </button>
-                        </td>
-                        <td>
-                            <button className="btn btn-danger">
-                                Delete
-                            </button>
-                        </td>
+    const handleDelete = async (id) => {
+        try {
+            const response = await RestaurantFinder.delete(`/${id}`);
+            setRestaurants(restaurants.filter(restaurant => {
+                return restaurant.restaurant_id !== id;
+            }))
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return (
+        <div className="list-group">
+            <table className="table table-striped table-dark">
+                <thead>
+                    <tr>
+                        <th scope="col">Restaurant</th>
+                        <th scope="col">Location</th>
+                        <th scope="col">Price Range</th>
+                        <th scope="col">Ratings</th>
+                        <th scope="col">Edit</th>
+                        <th scope="col">Delete</th>
                     </tr>
-                ))}
-            </tbody>
-        </table>
-    </div>
-  )
+                </thead>
+                <tbody>
+                    {restaurants && restaurants.map(restaurant => (
+                        <tr key={restaurant.restaurant_id}>
+                            <td>{restaurant.name}</td>
+                            <td>{restaurant.location}</td>
+                            <td>{"$".repeat(restaurant.price_range)}</td>
+                            <td>Rating</td>
+                            <td>
+                            <button className="btn btn-warning">
+                                Update
+                            </button>
+                            </td>
+                            <td>
+                                <button onClick={() => handleDelete(restaurant.restaurant_id)} className="btn btn-danger">
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default RestaurantList
